74d6743d848549b5e802f632856718796ae233ca
Buddhika Maitipe <brmaitipe@gmail.com>
2018-10-21 23:38:55 -0700

    Update

--- DataStructuresAndAlogorithms/src/ds/heap/MinHeap.java
+++ DataStructuresAndAlogorithms/src/ds/heap/MinHeap.java
@@ -26,16 +26,14 @@ public class MinHeap
     private int maxsize;
     private static final int FRONT = 1;//For ease of calculation root is taken as index 1
     
-    public MinHeap(int maxsize)
-    {
+    public MinHeap(int maxsize){
         this.maxsize = maxsize;
         this.currSize = 0;
         heapArr = new int[this.maxsize + 1];
         heapArr[0] = Integer.MIN_VALUE;
     }
 
-    public static void main(String arg[])
-    {
+    public static void main(String arg[]){
     	System.out.println("The MinHeap initializing ");
     	MinHeap minHeap = new MinHeap(15);
         minHeap.insert(17);
@@ -53,43 +51,33 @@ public class MinHeap
         System.out.println("Retrieve Minimum value: " + minHeap.remove());
     }
     
-    private int parent(int pos)
-    {
+    private int parent(int pos){
         return pos / 2;
     }
 
-    private int leftNode(int pos)
-    {
+    private int leftNode(int pos){
         return (2 * pos);
     }
 
-    private int rightNode(int pos)
-    {
+    private int rightNode(int pos) {
         return (2 * pos) + 1;
     }
 
-    private boolean isLeaf(int pos)
-    {
-        if (pos >=  (currSize / 2)  &&  pos <= currSize)
-        { 
+    private boolean isLeaf(int pos){
+        if (pos >=  (currSize / 2)  &&  pos <= currSize){ 
             return true;
         }
         return false;
     }
 
-    private void minHeapify(int pos)
-    {
-        if (!isLeaf(pos))
-        {
-            if ( heapArr[pos] > heapArr[leftNode(pos)] || heapArr[pos] > heapArr[rightNode(pos)])
-            {
-                if (heapArr[leftNode(pos)] < heapArr[rightNode(pos)])
-                {
+    private void minHeapify(int pos){
+        if (!isLeaf(pos)){
+            if ( heapArr[pos] > heapArr[leftNode(pos)] || heapArr[pos] > heapArr[rightNode(pos)]){
+                if (heapArr[leftNode(pos)] < heapArr[rightNode(pos)]){
                     swap(pos, leftNode(pos));
                     minHeapify(leftNode(pos));
                 }
-                else
-                {
+                else{
                     swap(pos, rightNode(pos));
                     minHeapify(rightNode(pos));
                 }
@@ -97,45 +85,36 @@ public class MinHeap
         }
     }
 
-    public void insert(int element)
-    {
+    public void insert(int element) {
         heapArr[++currSize] = element;
         int current = currSize;
-        while (heapArr[current] < heapArr[parent(current)])
-        {
+        while (heapArr[current] < heapArr[parent(current)]){
             swap(current,parent(current));
             current = parent(current);
         }	
     }
 
-    private void swap(int start, int end)
-    {
+    private void swap(int start, int end){
         int temp;
         temp = heapArr[start];
         heapArr[start] = heapArr[end];
         heapArr[end] = temp;
     }
     
-    public void print()
-    {
-        for (int i = 1; i <= currSize / 2; i++ )
-        {
+    public void print(){
+        for (int i = 1; i <= currSize / 2; i++ ){
             System.out.println("     Root Node : " + heapArr[i]);
             System.out.println(" Left Node: " + heapArr[2*i] + " Right Node :" + heapArr[2 * i  + 1]);
         } 
     }
 
-    public void minHeap()
-    {
-        for (int pos = (currSize / 2); pos >= 1 ; pos--)
-
-        {
+    public void minHeap(){
+        for (int pos = (currSize / 2); pos >= 1 ; pos--){
             minHeapify(pos);
         }
     }
 
-    public int remove()
-    {
+    public int remove(){
         int popped = heapArr[FRONT];
         heapArr[FRONT] = heapArr[currSize--]; 
         minHeapify(FRONT);// restructure heap
